ade3d6a1ed05e5937f006d111fd130c2f037510f
Atom Labs <atombaseinc@gmail.com>
2025-09-19 15:33:42 +0530

    Product integration

--- src/pages/admin/AdminProducts.tsx
+++ src/pages/admin/AdminProducts.tsx
@@ -11,10 +11,13 @@ import {
 import { useEffect, useState } from "react"
 import { getAllProducts } from "@/api/api"
 import { toast } from "sonner"
+import Loading from "@/components/Loading"
+import type { AdminProductType } from "@/types"
 
 
 const AdminProducts = () => {
-    const [products, setProducts] = useState(null)
+    const [products, setProducts] = useState<AdminProductType[]>([])
+    const [loading, setLoading] = useState(true)
 
     const fetchdata = async () => {
         try {
@@ -26,13 +29,16 @@ const AdminProducts = () => {
         } catch (error) {
             toast.error("Error while fetching Products")
         } finally {
-
+            setLoading(false)
         }
     }
     useEffect(() => {
         fetchdata()
     }, [])
 
+    if (loading) {
+        return <Loading />
+    }
     return (
         <div className="mt-4 border-2 round-sm h-full w-full flex justify-center items-start flex-col">
             <div className="flex w-[98svw] h-12 justify-between items-center text-lg shadow-lg px-2 text-primary">
@@ -45,37 +51,46 @@ const AdminProducts = () => {
                     </Button>
                 </div>
             </div>
-            <Table>
-                <TableHeader className="bg-primary ">
-                    <TableRow>
-                        <TableHead className="w-[100px] text-white">Name</TableHead>
-                        <TableHead className="text-white">Description</TableHead>
-                        <TableHead className="text-white">Category</TableHead>
-                        <TableHead className="text-white">Tags</TableHead>
-                        <TableHead className="text-white">Stock</TableHead>
-                        <TableHead className="text-white">Price</TableHead>
-                        <TableHead className="text-white text-right">Actions</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <TableRow>
-                        <TableCell className="font-medium">Test Product</TableCell>
-                        <TableCell>Test</TableCell>
-                        <TableCell>Test</TableCell>
-                        <TableCell>Credit,Card</TableCell>
-                        <TableCell>100</TableCell>
-                        <TableCell>100</TableCell>
-                        <TableCell className="flex w-full justify-end items-center gap-2">
-                            <Button className="bg-blue-600 hover:bg-blue-500 cursor-pointer">
-                                <Pencil />
-                            </Button>
-                            <Button className="bg-red-600 hover:bg-red-500 cursor-pointer">
-                                <Trash2 />
-                            </Button>
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
+            {(!loading && products.length === 0) ?
+                (
+                    <div> no products available </div>
+                ) : (
+                    <Table>
+                        <TableHeader className="bg-primary ">
+                            <TableRow>
+                                <TableHead className="w-[100px] text-white">Name</TableHead>
+                                <TableHead className="text-white">Description</TableHead>
+                                <TableHead className="text-white">Category</TableHead>
+                                <TableHead className="text-white">Tags</TableHead>
+                                <TableHead className="text-white">Stock</TableHead>
+                                <TableHead className="text-white">Price</TableHead>
+                                <TableHead className="text-white text-right">Actions</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {products.map((product, index) => (
+                                <TableRow key={index}>
+                                    <TableCell className="font-medium">{product.name}</TableCell>
+                                    <TableCell>{product.description}</TableCell>
+                                    <TableCell>{product.category}</TableCell>
+                                    <TableCell>{product.tags}</TableCell>
+                                    <TableCell>{product.stock}</TableCell>
+                                    <TableCell>{product.price}</TableCell>
+                                    <TableCell className="flex w-full justify-end items-center gap-2">
+                                        <Button className="bg-blue-600 hover:bg-blue-500 cursor-pointer">
+                                            <Pencil />
+                                        </Button>
+                                        <Button className="bg-red-600 hover:bg-red-500 cursor-pointer">
+                                            <Trash2 />
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                            }
+                        </TableBody>
+                    </Table>
+                )
+            }
         </div>
     )
 }
---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+
+
+type AdminProductType = {
+    id: string,
+    name: string,
+    description: string,
+    category: string,
+    tags: string,
+    price: number,
+    stock: number
+}
+
+export type { AdminProductType }
+
+//   "id": "string",
+//   "name": "string",
+//   "description": "string",
+//   "category": "string",
+//   "tags": "string",
+//   "price": 0,
+//   "stock": 0
