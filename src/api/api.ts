11bbef086ed8843fce2af9abe63a62a53a277c1c
Atom Labs <atombaseinc@gmail.com>
2025-09-19 23:41:46 +0530

    static auth patch

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,8 +2,11 @@ import axios from 'axios'
 
 const API = 'https://ecom-server-coxu.onrender.com'
 
+
+const authLogin = (logindata: any) => axios.post(`${API}/users/login`, logindata)
+const authRegister = (registerdata: any) => axios.post(`${API}/users/signup`, registerdata)
 const getAllProducts = () => axios.get(`${API}/products/all`)
 const addNewProduct = (product: any) => axios.post(`${API}/products/add`, product)
 const editProduct = (product: any, id: string) => axios.put(`${API}/products/product/edit/${id}`, product)
 const deleteProduct = (id: string) => axios.delete(`${API}/products/product/delete/${id}`)
-export { getAllProducts, addNewProduct, editProduct, deleteProduct }
+export { authLogin, authRegister, getAllProducts, addNewProduct, editProduct, deleteProduct }
